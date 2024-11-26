e9ece55ff67f338c8340594cc8d7c20307314545
latp1102 <latp1102@gmail.com>
2024-11-26 21:49:06 +0700

    Session06 - 5

---
+++ Session06-5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int year, month, day;
+
+    printf("Nhap vao so nam: ");
+    scanf("%d", &year);
+
+    printf("Nhap vao so thang: ");
+    scanf("%d", &month);
+
+    
+    int nam_nhuan = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+    
+    switch (month) {
+        case 1: 
+		case 3: 
+		case 5: 
+		case 7: 
+		case 8: 
+		case 10: 
+		case 12:
+            printf("co 31 ngay\n", month);
+            break;
+        case 4: 
+		case 6: 
+		case 9: 
+		case 11:
+            printf("co 30 ngay\n", month);
+            break;
+        case 2: 
+            printf("co 28 , 29 ngay\n", month);
+            break;
+        default:
+            printf("khong hop le\n");
+    }
+    printf("nam %d thang %d ngay %d\n", year, month, day);
+
+
+    return 0;
+}
