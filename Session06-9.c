9a97f00676146ef8046021a3df8915656cb271d9
latp1102 <latp1102@gmail.com>
2024-11-27 09:39:40 +0700

    Update Session06-9.c

--- Session06-9.c
+++ Session06-9.c
@@ -1,23 +1,23 @@
 #include<stdio.h>				
 int main(){				
-	int a,a1,tachSo,total,dem=0;			
-	a1=100;			
+	int a, b tachSo, total, dem=0;			
+	b = 100;			
 	while (1){			
-		a=a1;		
-		total=0;		
-		if (dem==4){		
+		a = b;		
+		total = 0;		
+		if (dem == 4){		
 			break;	
 		}		
-	while (a>0){			
-		tachSo=a%10;		
+	while (a > 0){			
+		tachSo = a % 10;		
 		total = total + (tachSo*tachSo*tachSo);		
-		a=a/10;		
+		a = a / 10;		
 		}		
-	if (total==a1){			
+	if (total == b){			
 		dem++;		
-		printf("%d ",a1);		
+		printf("%d ", b);		
 	}			
-	a1++;			
+	b++;			
 				
 	}			
 	return 0;			
